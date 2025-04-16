e4600f042084f5f5dd776ae8155854d5e194d8e3
Hazrat Ali <iloveyii@ayahoo.com>
2025-04-16 10:20:45 +0200

    feat: rabbit config

---
+++ src/main/java/com/learn/photo/rabbitmq/RabbitMqConfig.java
@@ -0,0 +1,40 @@
+package com.learn.photo.rabbitmq;
+
+import org.springframework.amqp.core.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing_key.name}")
+    private String routingKey;
+
+    // Bean for queue
+    @Bean
+    public Queue queue() {
+        return  new Queue(queue);
+    }
+
+    // Bean for exchange
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(exchange);
+    }
+
+    // Bean for binding key
+    public Binding binding() {
+        return BindingBuilder
+                .bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+}
