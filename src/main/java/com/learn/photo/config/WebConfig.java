964f07cb5aad19af64e493e1312690f2af9a2718
Hazrat Ali <iloveyii@ayahoo.com>
2025-04-21 07:27:03 +0200

    feat: props use forward headers, mvc config with CORS

--- src/main/java/com/learn/photo/config/WebConfig.java
+++ src/main/java/com/learn/photo/config/WebConfig.java
@@ -2,7 +2,8 @@ package com.learn.photo.config;
 
 import org.springframework.context.annotation.Configuration;
         import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-        import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -14,5 +15,13 @@ public class WebConfig implements WebMvcConfigurer {
                 .defaultContentType(org.springframework.http.MediaType.TEXT_HTML)
                 .mediaType("html", org.springframework.http.MediaType.TEXT_HTML)
                 .mediaType("json", org.springframework.http.MediaType.APPLICATION_JSON);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // allow all paths
+                .allowedOrigins("*") // or "*" or https://domain
+                .allowedMethods("*") // GET, POST, etc.
+                .allowCredentials(true);
     }
 }
