9b339d6b8de6555276f1004a2b5d265279d82d20
Hazrat Ali <iloveyii@ayahoo.com>
2025-06-20 07:23:00 +0200

    fix: comma

--- src/main/java/com/learn/photo/config/WebSocketConfig.java
+++ src/main/java/com/learn/photo/config/WebSocketConfig.java
@@ -29,8 +29,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         "https://spring.softhem.net:8080",  // Production domain
                         "http://192.168.*.*:8080",       // Internal network
                         "http://10.*.*.*:8080",       // Internal network
-                        "http://172.*.*.*:8080"      // Internal network,
-                        "http://127.*.*.*:8080"      // Internal network,
+                        "http://172.*.*.*:8080",      // Internal network,
+                        "http://127.*.*.*:8080",      // Internal network,
                         "http://127.*.*.*"      // Internal network,
 
                 )
