56c747b90ecd21c5bfc1bce59dfc97b664334dc3
Hazrat Ali <iloveyii@ayahoo.com>
2025-06-20 07:28:17 +0200

    fix: use dev

--- src/main/java/com/learn/photo/config/WebSocketConfig.java
+++ src/main/java/com/learn/photo/config/WebSocketConfig.java
@@ -19,21 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns(
-                        "http://localhost:*",       // Local dev
-                        "https://spring.softhem.net",  // Production domain
-                        "http://app:8080",     // Docker service name
-                        "http://192.168.*.*",       // Internal network
-                        "http://10.*.*.*",       // Internal network
-                        "http://172.*.*.*",      // Internal network
-                        "https://spring.softhem.net:8080",  // Production domain
-                        "http://192.168.*.*:8080",       // Internal network
-                        "http://10.*.*.*:8080",       // Internal network
-                        "http://172.*.*.*:8080",      // Internal network,
-                        "http://127.*.*.*:8080",      // Internal network,
-                        "http://127.*.*.*"      // Internal network,
-
-                )
+                .setAllowedOriginPatterns("*")  // Allow all in dev
                 .withSockJS();
     }
 
