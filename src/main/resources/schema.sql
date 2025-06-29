e2b85f5b6ad7ad9a4ab9b563f4c1c1074b4be3a9
Hazrat Ali <iloveyii@ayahoo.com>
2025-06-29 20:54:31 +0200

    feat: sql create product table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,3 +4,11 @@ CREATE TABLE IF NOT EXISTS photo (
     content_type VARCHAR(255),
     data BLOB
 );
+
+CREATE TABLE IF NOT EXISTS product (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    price FLOAT,
+    quantity INTEGER
+);
